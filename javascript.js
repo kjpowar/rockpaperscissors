e6da0e24032b7ca2dcfbce273324c11373dd18d3
kjpowar <kj.singh@icloud.com>
2024-08-09 20:55:28 +1000

    intial

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Computer will randomly choose rock, paper or scissors.
+console.log("test")
+// Human will select rock paper or scissors
+
+// Results of the game will be printed
+
+// Score will be added to the score board
+
+// It will go for 5 rounds then declare a winner
